27064aa20cc1894ef865f2784a06f4a350a3fa5c
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-06 17:08:46 +0530

    Update day1_pgm3.py

--- day1_pgm3.py
+++ day1_pgm3.py
@@ -1,5 +1,7 @@
 #Write a python program to add integers and floating point numbers.
 int_input = int(input("Enter an integer:"))
 float_input = float(input("Enter a float:"))
+add_int_int = int_input + int_input
+add_float_float = float_input + float_input
 add_int_float = int_input + float_input
-print(add_int_float)
+print(add_int_int, add_float_float, add_int_float)
