55b00a8c051896b6d0e001b0dc12f22ee7269247
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-06 17:28:50 +0530

    Update day1_pgm6.py

--- day1_pgm6.py
+++ day1_pgm6.py
@@ -1,6 +1,7 @@
 #Write a python program to reverse a string.
 string_input = input("Enter a string:")
-#reverse_string = string_input[::-1]
+#we can use below logic as well
+#reverse_string = string_input[::-1] ; print(reverse_string)
 string_list = list(string_input)
 string_list.reverse()
 string_input = "".join(string_list)
