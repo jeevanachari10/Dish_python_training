adbba6b3ee626bbcf6875f188455dbdccc27e417
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-07 15:47:41 +0530

    Create day2_pgm1.py

---
+++ day2_pgm1.py
@@ -0,0 +1,15 @@
+#Write a python program to display a paragraph containing “\b”,”\”,”\n” and alphanumeric characters.
+txt = 'It\'s alright.'
+print(txt)
+
+txt = "This will insert one \\ (backslash)."
+print(txt)
+
+txt = "Hello\nWorld!"
+print(txt)
+
+txt = "Hello\rWorld!"
+print(txt)
+
+txt = "Hello\tWorld!"
+print(txt) 
