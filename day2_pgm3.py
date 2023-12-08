799232f411536db6bc7bfaec7224f0460d7eb014
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-08 19:31:53 +0530

    Create day2_pgm3.py

---
+++ day2_pgm3.py
@@ -0,0 +1,6 @@
+#Create 5 lists. Each list has different objects. Combine first element of all list in one list, second element’s in another list and so on.
+list1 = [1, 2, 3, 4, 5]
+list2 = [6.1, 7.2, 8.3, 9.4, 10.5]
+list3 = ["apple", "banana", "cherry", "kiwi", "orange"]
+list4 = ['a', 'b', 'c', 'd', 'e']
+list5 = [True, False, True, False, True]
