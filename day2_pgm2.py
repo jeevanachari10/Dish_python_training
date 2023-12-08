ce369eb08e67372b0da1ce2c196191596af05a91
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-08 19:08:22 +0530

    Create day2_pgm2.py

---
+++ day2_pgm2.py
@@ -0,0 +1,16 @@
+def is_palindrome(s):
+    s = ''.join(e.lower() for e in s if e.isalnum())  #to get more accureate results few filters applied
+    return s == s[::-1]
+
+string_input = input("Enter a string:")
+#logic 1
+if string_input == string_input[::-1]:
+  print(f"{string_input} is a palindrome")
+else:
+  print(f"{string_input} is not a palindrome")
+
+#logic using funtions 
+if is_palindrome(string_input):
+    print(f"{string_input} is a palindrome")
+else:
+    print(f"{string_input} is not a palindrome")
