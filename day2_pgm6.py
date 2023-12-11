2aab9a52718bef9db171854273cc26677601013c
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-11 20:05:38 +0530

    Update day2_pgm6.py

--- day2_pgm6.py
+++ day2_pgm6.py
@@ -1,17 +1,16 @@
 #Write a python program to retrieve string which is repeated for more than once in the Paragraph. And store the resultant string to list.
-#--------------------------------Logic1-----------------------------------------------
+#------------logic1--------------without using any library--------------
 #import re
-mystring = "This is an example for the checking repeating words. We check the repeated words in this sentence. We will store the repeated words in a list"
+mystring = "This is an example for the checking repeating words. We check the repeated words in this sentence. We will store the repeated words in a list."
 
-delimiters = ".,;:-!"
+delimiters = ".,;:-!"    #defining delimiters we want to remove any punctuation
 words = mystring.translate(str.maketrans('', '', delimiters))
 words = words.lower()
-#words = re.sub(r'[^\w\s]', '', mystring.lower())
-#words = re.sub(r'\b\w+\b', '', mystring.lower())
-print(words)
-mystring_list = words.split()
-print(mystring_list)
-seen_words = set()
+#words = re.sub(r'[^\w\s]', '', mystring.lower()) #we can get the exct output importing regular expression library
+#print(words)
+mystring_list = words.split()    #converting sentence into list of words
+#print(mystring_list)
+seen_words = set()                #Using set as set won't allow duplicates
 repeated_words = set()
 for word in mystring_list:
     if word in seen_words:
@@ -19,28 +18,20 @@ for word in mystring_list:
     else:
         seen_words.add(word)
 
-print(list(repeated_words))
-#------------------------------------------------logic2----------------------------------------------
+print(list(repeated_words))        #printing in the form of list
+
+
+#-------------logic2--------------using libraries of regex and counter from collections-----------
 from collections import Counter
 import re
 
-def find_repeated_strings(paragraph):
-    # Split the paragraph into words using regex to handle punctuation
+def find_repeated_strings(sentence):
+    # Spliting the sentence into words using regex to handle punctuation
     words = re.findall(r'\b\w+\b', paragraph.lower())  # Using lowercase for case-insensitivity
-    
-    # Count occurrences of each word using Counter
-    word_count = Counter(words)
-    
-    # Find words repeated more than once
-    repeated_strings = [word for word, count in word_count.items() if count > 1]
-    
+    word_count = Counter(words)        # Count occurrences of each word using Counter
+    repeated_strings = [word for word, count in word_count.items() if count > 1]    # Find words repeated more than once
     return repeated_strings
 
-# Example paragraph
-paragraph = "This is a paragraph. This paragraph contains repeated words. Words like this are repeated."
-
-# Retrieve repeated strings
-result_list = find_repeated_strings(paragraph)
-
-print("Strings repeated more than once in the paragraph:")
+mystring = "This is an example for the checking repeating words. We check the repeated words in this sentence. We will store the repeated words in a list."
+result_list = find_repeated_strings(mystring)
 print(result_list)
