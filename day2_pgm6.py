1680de6c1f3b5494a7733bd71b18f762bedec0a3
jeevanachari10 <152246289+jeevanachari10@users.noreply.github.com>
2023-12-12 11:51:41 +0530

    Update day2_pgm6.py

--- day2_pgm6.py
+++ day2_pgm6.py
@@ -1,12 +1,10 @@
 #Write a python program to retrieve string which is repeated for more than once in the Paragraph. And store the resultant string to list.
 #------------logic1--------------without using any library--------------
-#import re
 mystring = "This is an example for the checking repeating words. We check the repeated words in this sentence. We will store the repeated words in a list."
 
 delimiters = ".,;:-!"    #defining delimiters we want to remove any punctuation
 words = mystring.translate(str.maketrans('', '', delimiters))
 words = words.lower()
-#words = re.sub(r'[^\w\s]', '', mystring.lower()) #we can get the exct output importing regular expression library
 #print(words)
 mystring_list = words.split()    #converting sentence into list of words
 #print(mystring_list)
@@ -28,6 +26,7 @@ import re
 def find_repeated_strings(sentence):
     # Spliting the sentence into words using regex to handle punctuation
     words = re.findall(r'\b\w+\b', paragraph.lower())  # Using lowercase for case-insensitivity
+    #words = re.sub(r'[^\w\s]', '', mystring.lower()) #also use sub fucn
     word_count = Counter(words)        # Count occurrences of each word using Counter
     repeated_strings = [word for word, count in word_count.items() if count > 1]    # Find words repeated more than once
     return repeated_strings
